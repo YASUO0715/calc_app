ef79add05102e0787292790889149a6d8806b33b
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-14 16:16:20 +0900

    課題再修正

--- app/Http/Controllers/calcsController.php
+++ app/Http/Controllers/calcsController.php
@@ -9,30 +9,21 @@ class calcsController extends Controller
     public function word($num1, $function, $num2)
     {
 
-    if(
-        $function 
-        === 'addtion'
-        ){
-        $function = $num1 + $num2;
-    }elseif(
-        $function 
-        === 'subtraction'
-        ){
-        $function = $num1 - $num2;
-    }elseif(
-        $function 
-        === 'multiplication'
-        ){
-        $function = $num1 * $num2;
-    }else
-    {
-        $function = $num1 / $num2;
-    }
+        if ($function === 'addtion') {
+            $function = $num1 + $num2;
+        } elseif ($function === 'subtraction') {
+            $function = $num1 - $num2;
+        } elseif ($function === 'multiplication') {
+            $function = $num1 * $num2;
+        } else {
+            $function = $num1 / $num2;
+        }
 
-        return view('message.word',
-        [
-        'function'=> $function,
-            ]);
+        return view(
+            'message.word',
+            [
+                'function' => $function,
+            ]
+        );
     }
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/controller/word/{num1}/{function}/{num2}', [App\Http\Controllers\calcsController::class,'word']);
+Route::get('/controller/word/{num1}/{function}/{num2}', [App\Http\Controllers\calcsController::class, 'word']);
